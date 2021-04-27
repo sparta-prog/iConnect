c964bd6cc69e37f6eed9ea73accbdc2b00824dbe
debarun <debarun285@gmail.com>
2021-04-27 10:10:57 +0530

    final

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const io = socketio(server);
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-const botName = 'JARVIS';
+const botName = 'Jarvis';
 
 // Run when client connects
 io.on('connection', socket => {
